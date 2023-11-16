5e53b602a8f2a9ebb42bbdcf12319d8cedabb8dd
scholastic-superuser <140845400+scholastic-superuser@users.noreply.github.com>
2023-11-16 06:06:51 +0000

    learnMySQL.sql was committed to Git.

---
+++ Coding/learnMySQL.sql
@@ -0,0 +1,18 @@
+CREATE TABLE  learningMySQL(
+    id INTEGER,
+    name TEXT,
+    year INTERGER,
+    distribution_company TEXT
+);
+
+INSERT INTO learningMySQL VALUES(1, "The Godfather Part I", 1972, "Paramount Pictures");
+INSERT INTO learningMySQL VALUES(2, "The Godfather Part II", 1974, "Paramount Pictures");
+INSERT INTO learningMySQL VALUES(3, "Taxi Driver", 1976, "Columbia Pictures");
+INSERT INTO learningMySQL VALUES(4, "Inception", 2010, "Warner Bros.");
+INSERT INTO learningMySQL VALUES(5, "Everything Everywhere All At Once", 2022, "A24 Films");
+INSERT INTO learningMySQL VALUES(6, "Akira", 1988, "Toho Co.");
+INSERT INTO learningMySQL VALUES(7, "Whiplash", 2014, "Sony Pictures");
+INSERT INTO learningMySQL VALUES(8, "12 Angry Men", 1957, "Metro-Goldwyn-Mayer");
+INSERT INTO learningMySQL VALUES(9, "The Wizard of Oz", 1939, "Metro-Goldwyn-Mayer");
+INSERT INTO learningMySQL VALUES(10, "Donnie Darko", 2001, "independent");
+INSERT INTO learningMySQL VALUES(11, "Requiem for a Dream", 2000, "independent");
