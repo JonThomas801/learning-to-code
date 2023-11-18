c76f9dd2bcab028de64c7cee3aac2240dcdb0f46
scholastic-superuser <140845400+scholastic-superuser@users.noreply.github.com>
2023-11-18 00:41:08 +0000

    deleted "decrypt" section

--- caesar.py
+++ caesar.py
@@ -2,7 +2,10 @@
 
 print("\n###  Welcome to Caesar.py!  ###\n")
 
-def encrypt_text(plaintext,n):
+cryptType = input("Would you like to encrypt or decrypt? ")
+
+if "d" not in cryptType.lower():
+    def encrypt_text(plaintext,n):
     ans = ""
     # iterate over the given text
     for i in range(len(plaintext)):
@@ -20,45 +23,44 @@ def encrypt_text(plaintext,n):
             ans += chr((ord(ch) + n-97) % 26 + 97)
     
     return ans
+else
+    plaintext = input("Enter plaintext here: ")
+    n = int(input("Enter shift value here (1-26): "))
+    print("Ciphertext: " + encrypt_text(plaintext,n))
 
-plaintext = input("Enter plaintext here: ")
-n = int(input("Enter shift value here (1-26): "))
-print("Ciphertext: " + encrypt_text(plaintext,n))
-
-upLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-downLetters = upLetters.lower()
-for key in range(len(Letters)):
-   translated = ''
-   for ch in message:
-      if ch in Letters:
-         num = Letters.find(ch)
-         num = num - key
-         if num < 0:
-            num = num + len(Letters)
-         translated = translated + Letters[num]
-      else:
-         translated = translated + ch
-   print('Hacking key is %s: %s' % (key, translated))
-
-def decrypt():
-    
-    #enter your encrypted message(string) below
-    encrypted_message = input("Enter the message i.e to be decrypted: ").strip()
-    
-    #enter the key value to decrypt
-    k = int(input("Enter the key to decrypt: "))
-    decrypted_message = ""
-
-    for ch in encrypted_message:
-
+    Letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    letters = Letters.lower()
+    for key in range(len(letters)):
+    translated = ''
+    for ch in message:
         if ch in letters:
-            position = letters.find(ch)
-            new_pos = (position - k) % 26
-            new_char = letters[new_pos]
-            decrypted_message += new_char
+            num = letters.find(ch)
+            num = num - key
+            if num < 0:
+                num = num + len(letters)
+            translated = translated + letters[num]
         else:
-            decrypted_message += ch
-    print("Your decrypted message is:\n")
-    print(decrypted_message)
+            translated = translated + ch
+    print('Hacking key is %s: %s' % (key, translated))
 
-decrypt()
+    def decrypt():
+        
+        #enter your encrypted message(string) below
+        encrypted_message = input("Enter the message i.e to be decrypted: ").strip()
+        
+        #enter the key value to decrypt
+        k = int(input("Enter the key to decrypt: "))
+        decrypted_message = ""
+
+        for ch in encrypted_message:
+
+            if ch in letters:
+                position = letters.find(ch)
+                new_pos = (position - k) % 26
+                new_char = letters[new_pos]
+                decrypted_message += new_char
+            else:
+                decrypted_message += ch
+        print("Your decrypted message is:\n")
+        print(decrypted_message)
+    decrypt()
