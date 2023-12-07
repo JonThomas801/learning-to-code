de811ad4205424f3cadbe4fc7b46e39d2c0d02e1
scholastic-superuser <140845400+scholastic-superuser@users.noreply.github.com>
2023-12-07 16:04:24 -0700

    added concatenation to learnLua.lua

--- Coding/learnLua.lua
+++ Coding/learnLua.lua
@@ -6,3 +6,6 @@ And this is a multi-lined comment!
 ]]
 
 print(type("What will this type?"))
+
+variable1 = "Concatenation!"
+print("What is it called when two different strings are put together? " .. variable1)
