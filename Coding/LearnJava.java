98cbec740d75cda4ff957f8c40d250c38ed7dad6
scholastic-superuser <140845400+scholastic-superuser@users.noreply.github.com>
2024-01-06 20:58:34 +0000

    Update and rename learnJava.java to LearnJava.java

---
+++ Coding/LearnJava.java
@@ -0,0 +1,14 @@
+//The "Java Virtual Machine" or "JVM" for short makes it so Java can compile the same exact syntax on different operating systems.
+
+public class LearnJava {
+//This establishes a class, a blueprint or template for an object in Java.
+  public static void main(String[] args) {
+  //This is a main() method.
+    System.out.println("Hi mom!");
+    /* "System" is a built-in Java class.
+    "out" is short for "output"
+    "println" is short for "print line" */
+    System.out.print("This sentence");
+    System.out.print("is false!"); // Using "print" instead of "println"
+  }
+}
