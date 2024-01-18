f431f7656c5309cad3ee4b0d117c9c42e8c66f2d
scholastic-superuser <140845400+scholastic-superuser@users.noreply.github.com>
2024-01-18 16:20:24 -0700

    Added primitive and string variables into LearnJava.java

--- Coding/LearnJava.java
+++ Coding/LearnJava.java
@@ -12,6 +12,16 @@ public class LearnJava {
     System.out.print("is false!"); // Using "print" instead of "println"
 
     //Primitive data types are the most basic data types that are built into the system, such as int, double, and boolean
-    
+
+    int yearGitWasFounded = 2005;
+    double piValue = 3.1415928;
+    boolean isJavaOOP = true;
+    char expectedGrade = 'D';
+    //Strings are *objects* as opposed to primitives
+    String chorusLine = "If I go crazy then will you still call me Superman?"; //This is a String literal
+    String chorusLine2 = new String ("If I'm alive and well, will you be there holding my hand?"); //This calls the String class to create a new string object
+    /*
+    Escape characters list
+    */
   }
 }
