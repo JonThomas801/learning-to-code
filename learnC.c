0a47833ce2ae19af79c0534da8e4787f04b49dda
scholastic-superuser <wolfgangthomas64@gmail.com>
2024-01-21 04:35:57 -0700

    modified:   .vscode/c_cpp_properties.json 	modified:   .vscode/launch.json 	renamed:    learn_C++.cpp -> learnC++.cpp 	modified:   learnC.c

--- learnC.c
+++ learnC.c
@@ -1,5 +1,12 @@
-#include <stdio.h>
-int main() {
-    printf("Hi mom!");
-    return 0;
+#include <stdio.h> //This is the header file library
+int main() { //Any and all code in the curly brackets of the main() function with be executed
+    printf("Hi mom!\n"); //The escape sequence \n creates a new line
+    int myNum = 60;
+    printf("%d\n", myNum); // "%d" is a format specifier for int variables, which act as placeholders
+    return 0; //Ends the program successfully
 }
+
+/* To compile and run this program, type to following into the terminal:
+$ gcc learnC.c -o learnC
+$ ./learnC
+*/
