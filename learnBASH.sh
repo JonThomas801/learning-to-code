e0f0f473a533e40602e12dcd3314075a6a42012d
scholastic-superuser <wolfgangthomas64@gmail.com>
2024-01-30 01:25:50 -0700

    Add learnBASH.sh

---
+++ learnBASH.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# The pound sign plus the exclamation point make a sha-bang, then showing the directory where the Shell language is.
+# For this program, we will be using BASH, the Bourne-Again Shell.
+# To run, type in "chmod +x learnBASH.sh" then "./learnBASH.sh"
+# To discover what shell you are using, type in "ps || grep $$"
+echo "Hello world!"
+echo 'Welcome to learnBASH.sh!'
