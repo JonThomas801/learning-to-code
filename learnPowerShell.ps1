bb649e023a7632c33f9a7ce2693d259e0741c42c
JonThomas801 <140845400+JonThomas801@users.noreply.github.com>
2024-04-26 13:05:14 +0000

    Update learnPowerShell.ps1

--- learnPowerShell.ps1
+++ learnPowerShell.ps1
@@ -4,3 +4,13 @@ Write-Host "Welcome to learnPowerShell.ps1!"
 $name = Read-Host -Prompt "What is your name?"
 
 [Int]$num_1 = 42
+[String]$num_2 = "forty two"
+$name, $color, $date = "Paul", "chartreuse", (Get-Date).DateTime
+$date_data_type = $date.GetType().Name
+# Initialize variables above
+
+Write-Host "Value of num_1 is $num_1"
+Write-Host "Hello, my name is $name!"
+Write-Host "My favorite color is $color."
+Write-Host "Today is $date"
+Write-Host "The data type for 'date' is '$date_data_type'`n"
